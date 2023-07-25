bcf00221b9a9b7b901e07d715b99662241f359e9
Githu77 <githubenard16@gmail.com>
2023-07-25 09:27:25 +0300

    add bubble_sort file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+* bubble_sort - sorts integers by bubbling the big numbers to the right
+* @array: integers
+* @size: array size
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, x;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (x = 0; x < size - 1; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				temp = array[x];
+				array[x] = array[x + 1];
+				array[x + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
